8a4d3ced9b03037d25f82ac182a5cf9ee9cd4818
Fergal <fergaljosullivan@gmail.com>
2020-04-28 15:10:05 +0200

    Added verb data for sharing.

---
+++ verb-data-1.js
@@ -0,0 +1,38 @@
+// Verb Data
+            var data = { 
+            version: 1,
+            title: 'Verb endings',
+            text: [
+    
+            {tenseTitle:"Person", conjugationsCount:1, conjugations: [
+            {conjugTitle:"", conjugValues:["yo","tú", "el", "nostros", "vostros", "ellos" ] }
+            
+            ]},
+    
+            {tenseTitle:"Imperfecto", conjugationsCount:2, conjugations: [
+            {conjugTitle:"AR", conjugValues:["aba","abas", "aba", "ábamos", "abais", "aban" ] },
+            {conjugTitle:"ER/IR", conjugValues:["ía","ías", "ía", "íamos", "íais", "ían" ] }
+            ]},
+    
+            {tenseTitle:"Preterito", conjugationsCount:3, conjugations: [
+            {conjugTitle:"AR", conjugValues:["é","aste", "ó", "amos", "asteis", "aron" ] },
+            {conjugTitle:"ER/IR", conjugValues:["í","iste", "ió", "imos", "isteis", "ieron" ] },
+            {conjugTitle:"Irreg", conjugValues:["e","iste", "o", "imos", "isteis", "ieron" ] }
+            ]},
+    
+            {tenseTitle:"Presento", conjugationsCount:3, conjugations: [
+            {conjugTitle:"AR", conjugValues:["o","as", "a", "amos", "áis", "an" ] },
+            {conjugTitle:"ER", conjugValues:["o","es", "e", "emos", "éis", "en" ] },
+            {conjugTitle:"IR", conjugValues:["o","es", "e", "imos", "ís", "en" ] }
+            ]},
+    
+            {tenseTitle:"Futuro", conjugationsCount:1, conjugations: [
+            {conjugTitle:"AR/ER/IR", conjugValues:["é","ás", "á", "emos", "éis", "án" ] },
+            ]},
+    
+            {tenseTitle:"Condicional", conjugationsCount:1, conjugations: [
+            {conjugTitle:"AR/ER/IR", conjugValues:["ía","ías", "ía", "íamos", "íais", "ían" ] }   
+            ]},
+    
+            ]
+            };
